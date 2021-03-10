bd02c6c4fddd85d465db0c55654e6f3106548b43
oddwes <oddwes777@gmail.com>
2021-03-10 05:41:11 -0500

    adds default values to home page to indicate loading

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,13 @@ import { getUser, getActivities } from "../utils/PizzlyUtil"
 import { beginningOfMonth } from "../utils/DateUtil"
 
 const Home = () => {
-  const [username, setUsername] = useState([])
-  const [activityCount, setActivityCount] = useState([])
+  const [username, setUsername] = useState("_")
+  const [activityCount, setActivityCount] = useState("_")
 
   useEffect(() => {
     getUser().then(response => response.json().then(object => setUsername(`${object.firstname} ${object.lastname}`)))
     getActivities({after: beginningOfMonth().getTime()/1000}).then(response => response.json().then(object => setActivityCount(object.length)))
-  })
+  }, [])
 
   return (
     <React.Fragment>
