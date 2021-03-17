8dd1909b10277281800a311c3afefc28f41c1fde
oddwes <oddwes777@gmail.com>
2021-03-17 01:54:20 -0400

    ditch react-vis => use chartjs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from "react"
 import { getAuthId } from "../utils/PizzlyUtil"
 import { getUser, getActivities } from "../utils/StravaUtil"
-import { beginningOfMonth } from "../utils/DateUtil"
+import { monthNames, monthIndices, beginningOfMonth } from "../utils/DateUtil"
 import DatePicker from "react-datepicker"
 import { Activities } from "./Activities"
-import { RadialChart, Hint } from "react-vis"
+import { Doughnut, Line } from 'react-chartjs-2';
 
 import "react-datepicker/dist/react-datepicker.css";
+
 
 const Home = () => {
   const [username, setUsername] = useState("_")
   const [activities, setActivities] = useState([])
   const [startDate, setStartDate] = useState(beginningOfMonth())
   const [endDate, setEndDate] = useState(new Date())
-  const [value, setValue] = useState(false)
 
   useEffect(() => {
     getUser({ pizzly_auth_id: getAuthId() }).then(response => setUsername(response.data.athlete.first_name))
@@ -21,16 +21,57 @@ const Home = () => {
 
   useEffect(() => {
     getActivities({ pizzly_auth_id: getAuthId(), after: startDate, before: endDate })
-      .then(response => {
-        if (response.status === 200) {
-          const sortedActivities = response.data.activities.sort(function (a, b) { return new Date(b.created_at) - new Date(a.created_at) })
-          setActivities(sortedActivities)
-        }
-      })
+    .then(response => {
+      if (response.status === 200) {
+        const sortedActivities = response.data.activities.sort(function (a, b) { return new Date(b.created_at) - new Date(a.created_at) })
+        setActivities(sortedActivities)
+      }
+    })
   }, [startDate, endDate])
 
   const updateStartDate = ({ startDate }) => {
     setStartDate(startDate)
+  }
+
+  const data = {
+    labels: ['Indoor Rides', 'Outdoor Rides'],
+    datasets: [
+      {
+        label: '# of Rides',
+        data: [
+          activities.filter((activity) => activity.data.trainer === true).length,
+          activities.filter((activity) => activity.data.trainer === false).length,
+        ],
+        backgroundColor: [
+          "#E7F8F3",
+          "#14939A",
+        ],
+        borderColor: [
+          "#14939A", 'grey'
+        ],
+        borderWidth: 1,
+      },
+    ],
+  }
+
+  const goalData = {
+    labels: monthNames,
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+      },
+      {
+        label: '# of People',
+        data: [1, 9, 13, 32, 2, 3],
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+      },
+    ],
   }
 
   return (
@@ -46,39 +87,8 @@ const Home = () => {
       <DatePicker selected={startDate} maxDate={endDate} onChange={date => updateStartDate({ startDate: date })} />
       <DatePicker selected={endDate} minDate={startDate} onChange={date => setEndDate(date)} />
       <Activities activities={activities} />
-      <div>
-        <RadialChart
-          innerRadius={80}
-          radius={140}
-          data={
-            [
-              {
-                // label: 'Indoor Rides',
-                // subLabl: activities.filter((activity) => activity.data.trainer === true).length / activities.length,
-                activityCount: activities.filter((activity) => activity.data.trainer === true).length,
-                color: "#E7F8F3",
-                angle: activities.filter((activity) => activity.data.trainer === true).length / activities.length,
-              },
-              {
-                angle: activities.filter((activity) => activity.data.trainer === false).length / activities.length,
-                activityCount: activities.filter((activity) => activity.data.trainer === false).length,
-                // label: "Outdoor Rides",
-                color: "#14939A",
-              },
-            ]
-          }
-          width={300}
-          height={300}
-          showLabels={true}
-          colorType="literal"
-          onValueMouseOver={v => setValue({ activities: v.activityCount })}
-          onSeriesMouseOut={() => setValue(false)}
-          padAngle={0.04}
-        >
-          {value !== false && <Hint value={value} className="rv-hint" />}
-        </RadialChart>
-
-      </div>
+      <Doughnut data={data} />
+      <Line data={goalData} />
     </React.Fragment>
   )
 }
--- src/utils/DateUtil.js
+++ src/utils/DateUtil.js
@@ -1,3 +1,6 @@
+export const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+export const monthIndices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
 export const beginningOfMonth = (date) => {
   date = date || new Date();
   return new Date(date.getFullYear(), date.getMonth(), 1);
