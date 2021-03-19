b009a8907506496b59dfa44a071694c9610268be
oddwes <oddwes777@gmail.com>
2021-03-19 02:01:32 -0400

    updates footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,11 @@ import React, {useState} from 'react';
 import '../styling/footer.css';
 
 const Footer = ({text, url}) => {
-  const [footerText] = useState(url ? <a target="_blank" rel="noopener noreferrer" href={url}>{text}</a> : text);
+  const [footerText] = useState(url ? <a target="_blank" rel="noopener noreferrer" href={url} style={{textDecoration: "none"}}>{text}</a> : text);
 
   return(
     <div className="footer">
+      <hr/>
       <p style={{textAlign: 'center'}}>
         {footerText}
       </p>
