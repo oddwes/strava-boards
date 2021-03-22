a247c5ad635fa30bb6ddf278c5c75a3095d6d850
oddwes <oddwes777@gmail.com>
2021-03-21 22:07:03 -0400

    fixes year filtering in goals tab

--- src/utils/StravaUtil.js
+++ src/utils/StravaUtil.js
@@ -6,6 +6,6 @@ export const getUser = ({ pizzly_auth_id }) => {
   return axios.get(`${CACHE_HOST}/athlete`, { headers: { "Pizzly-Auth-Id": pizzly_auth_id } })
 }
 
-export const getActivities = ({ pizzly_auth_id, after }) => {
-  return axios.get(`${CACHE_HOST}/athlete/activities`, { params: { after: after }, headers: { "Pizzly-Auth-Id": pizzly_auth_id } })
+export const getActivities = ({ pizzly_auth_id, after, before }) => {
+  return axios.get(`${CACHE_HOST}/athlete/activities`, { params: { after: after, before: before }, headers: { "Pizzly-Auth-Id": pizzly_auth_id } })
 }
